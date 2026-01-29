f9891d1ff7414ab876e28f96bc3b8e912e10c7bb
Ryanpan0711 <az855616@gmail.com>
2026-01-29 21:32:03 +0800

    修改

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 echo "Building project with Maven..."
 mvn clean package -DskipTests
 
-echo "Starting Tomcat embedded server..."
+echo "Starting Jetty server..."
 # Use the PORT environment variable if provided by the platform
 PORT=${PORT:-8080}
-java -jar target/attendances-project-exec.war --server.port=$PORT
+java -jar target/dependency/jetty-runner.jar --port $PORT target/attendances-project.war
